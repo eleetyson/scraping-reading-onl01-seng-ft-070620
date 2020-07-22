d61ae8af561341e4e96662c1a858442f816cf159
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-22 16:38:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,4 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html) # take the string, convert it into a NodeSet, store in doc
 
-doc.css(".headline-260IBN")
+doc.css(".headline-260IBN").text
